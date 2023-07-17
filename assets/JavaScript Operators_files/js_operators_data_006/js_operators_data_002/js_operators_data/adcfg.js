94d9a26ecdfb392ecbf1ad3b0094bd95de6371c0
fringemonkey89 <markomajeticjr@gmail.com>
2023-07-17 19:14:12 -0400

    removed erroneous javascript file

--- assets/JavaScript Operators_files/js_operators_data_006/js_operators_data_002/js_operators_data/adcfg.js
+++
@@ -1,1 +0,0 @@
-sovrn.ads.renderAd({"w":300,"h":250,"iid":"13410076","dv":0,"geo":0,"rl":1,"gdprApplies":0,"ccpaApplies":0,"tid":"a_662029_688f23e116f747419e87ea1d49af72f6","webUserName":"carambola2"});
